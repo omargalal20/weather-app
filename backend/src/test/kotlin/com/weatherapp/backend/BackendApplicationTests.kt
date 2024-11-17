ca29127cecac23494634d6ab3b15bd94f53aad23
omargalal20 <oelhanafy12@gmail.com>
2024-11-16 20:22:42 -0800

    chore(backend): setted profile test for spring test

--- backend/src/test/kotlin/com/weatherapp/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/weatherapp/backend/BackendApplicationTests.kt
@@ -3,11 +3,9 @@ package com.weatherapp.backend
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(properties = ["spring.profiles.active=test"])
 class BackendApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
